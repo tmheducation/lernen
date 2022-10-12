e555dd679d768c73904f27470beb690014aac8ca
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-12 11:06:20 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-	
-})
 let Eingabe = 0
 if (input.buttonIsPressed(Button.AB)) {
     Eingabe = 2
+}
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Butterfly)
+basic.showIcon(IconNames.Meh)
+if (Eingabe == 2) {
+    led.plot(1, 2)
 }
 basic.forever(function () {
 	
