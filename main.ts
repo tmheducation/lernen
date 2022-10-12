717e7e1e42f995bda6b057b6d396cf7132ecb41f
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-12 11:00:20 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+	
+})
+let Eingabe = 0
+if (input.buttonIsPressed(Button.AB)) {
+    Eingabe = 2
+}
+basic.forever(function () {
+	
+})
